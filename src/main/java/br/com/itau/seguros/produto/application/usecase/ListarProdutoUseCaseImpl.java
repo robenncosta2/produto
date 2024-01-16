6f72820528a9aa04059f37aef373edaea525b472
Roben Costa <robenncosta@gmail.com>
2024-01-16 02:11:01 -0300

    refactor: remove import não utilizado

--- src/main/java/br/com/itau/seguros/produto/application/usecase/ListarProdutoUseCaseImpl.java
+++ src/main/java/br/com/itau/seguros/produto/application/usecase/ListarProdutoUseCaseImpl.java
@@ -2,7 +2,6 @@ package br.com.itau.seguros.produto.application.usecase;
 
 import br.com.itau.seguros.produto.application.gateway.ProdutoGateway;
 import br.com.itau.seguros.produto.domain.model.Produto;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -19,3 +18,4 @@ public class ListarProdutoUseCaseImpl implements ListarProdutosUseCase {
         return produtoGateway.findAllProdutos();
     }
 }
+ 
